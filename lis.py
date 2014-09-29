bc0070ef2a889d6025a2fe37b8044a4b55ac980b
Connor Taffe <cpaynetaffe@gmail.com>
2014-09-28 20:18:10 -0500

    error catching, verbose & symbolic names for all builtins

--- lis.py
+++ lis.py
@@ -12,6 +12,22 @@ class lispy(object):
 		else:
 			f = codecs.open(self.fn, encoding='utf-8')
 		p = parse.Parse(lex.Lex(scan.Scan(f)))
-		for t in optim.Eval(p):
-			print pprint.pprint(t).pprint()
+		for t in lispy_eval(p):
+			if not t == None:
+				print pprint.pprint(t).pprint()
 		return self
+
+# error catching iterator
+class lispy_eval(object):
+	def __init__(self, p):
+		self.eval = optim.Eval(p).__iter__()
+	def __iter__(self):
+		return self
+	def next(self):
+		try:
+			return self.eval.next()
+		except Exception as e:
+			# EOF
+			if len(str(e)) == 0:
+				exit()
+			print "err: " + str(e)
